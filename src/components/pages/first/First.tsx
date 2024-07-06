96510a315eba8cc158be1a02f18fe858167f25b3
Nursultan <kydyrmysovnursultan@gmail.com>
2024-07-06 18:44:21 +0600

    correct first page

--- src/components/pages/first/First.tsx
+++ src/components/pages/first/First.tsx
@@ -1,12 +1,23 @@
+import Arrow from "../../../assets/icon/Arrow";
 import scss from "./First.module.scss";
 const First = () => {
   return (
-    <div className={scss.First}>
-      <img
-        src="https://assets.nflxext.com/ffe/siteui/vlv3/335ddde7-3955-499c-b4cc-ca2eb7e1ae71/445c9c3b-6a60-4c0f-baa2-3c6cc3f0e557/KG-ru-20240624-POP_SIGNUP_TWO_WEEKS-perspective_WEB_4107af1d-602c-4884-a714-c19620373931_large.jpg"
-        alt=""
-      />
-    </div>
+    <>
+      <div className={scss.First}></div>
+      <div className={scss.content}>
+        <div>
+          <h1>
+            Фильмы, сериалы и многое другое без <br /> ограничений
+          </h1>
+          <h2>Смотрите где угодно. Отменить подписку можно в любое время.</h2>
+        </div>
+        <div className={scss.buttons}>
+          <button>Далее</button>
+          {/* <IconChevronsRight /> */}
+          <Arrow />
+        </div>
+      </div>
+    </>
   );
 };
 
